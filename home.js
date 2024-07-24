add8607648195f8a3bb3a4d1d0253435416b1b02
Minesha Kodithuwakku <mineshakodithuwakku100@gmail.com>
2024-07-24 13:24:30 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,34 @@
+onload=window.alert('Hello Welcome to Village Ceylon!!!');  
+
+
+
+function first(){
+    document.getElementById('kithulimage').style.display = 'block';
+    document.getElementById('cinnomon').style.display = 'none';
+    document.getElementById('cardamom').style.display = 'none';
+    document.getElementById('blackpaper').style.display = 'none';
+}
+
+function second(){
+    document.getElementById('kithulimage').style.display = 'none';
+    document.getElementById('cinnomon').style.display = 'block';
+    document.getElementById('cardamom').style.display = 'none';
+    document.getElementById('blackpaper').style.display = 'none';
+}
+
+
+function third(){
+    document.getElementById('kithulimage').style.display = 'none';
+    document.getElementById('cinnomon').style.display = 'none';
+    document.getElementById('cardamom').style.display = 'block';
+    document.getElementById('blackpaper').style.display = 'none';
+}
+function fourth(){
+    document.getElementById('kithulimage').style.display = 'none';
+    document.getElementById('cinnomon').style.display = 'none';
+    document.getElementById('cardamom').style.display = 'none';
+    document.getElementById('blackpaper').style.display = 'block';
+}
+
+
+
